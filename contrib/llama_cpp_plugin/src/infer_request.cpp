927e2ba84491c73e85dbd69c9ec30837894b0592
Vasily Shamporov <vasily.shamporov@intel.com>
2024-02-08 19:34:16 +0100

    Now stub-infers after allocating input and output tensors

--- contrib/llama_cpp_plugin/src/infer_request.cpp
+++ contrib/llama_cpp_plugin/src/infer_request.cpp
@@ -1,9 +1,38 @@
 #include "infer_request.hpp"
+#include "openvino/runtime/make_tensor.hpp"
 
 namespace ov {
     namespace llama_cpp_plugin {
+
+        void allocate_tensor_impl(ov::SoPtr<ov::ITensor>& tensor,
+                          const ov::element::Type& element_type,
+                          const ov::Shape& shape) {
+    if (!tensor || tensor->get_element_type() != element_type) {
+        tensor = ov::make_tensor(element_type, shape);
+    } else {
+        tensor->set_shape(shape);
+    }
+}
+
         LlamaCppSyncInferRequest::LlamaCppSyncInferRequest(const std::shared_ptr<const LlamaCppModel>& compiled_model): ov::ISyncInferRequest(compiled_model) {
             std::cout << "VSHAMPOR: infer request ctor called\n";
+            // Allocate input/output tensors
+            for (const auto& input : get_inputs()) {
+                allocate_tensor(input, [input](ov::SoPtr<ov::ITensor>& tensor) {
+                    // Can add a check to avoid double work in case of shared tensors
+                    allocate_tensor_impl(tensor,
+                                         input.get_element_type(),
+                                         input.get_partial_shape().is_dynamic() ? ov::Shape{0} : input.get_shape());
+                });
+            }
+            for (const auto& output : get_outputs()) {
+                allocate_tensor(output, [output](ov::SoPtr<ov::ITensor>& tensor) {
+                    // Can add a check to avoid double work in case of shared tensors
+                    allocate_tensor_impl(tensor,
+                                         output.get_element_type(),
+                                         output.get_partial_shape().is_dynamic() ? ov::Shape{0} : output.get_shape());
+                });
+    }
         }
     void LlamaCppSyncInferRequest::set_tensors_impl(const ov::Output<const ov::Node> port, const std::vector<ov::SoPtr<ov::ITensor>>& tensors) {
         std::cout << "VSHAMPOR: set_tensors_impl called\n";
@@ -11,6 +40,8 @@ namespace ov {
 
     void LlamaCppSyncInferRequest::infer() {
         std::cout << "VSHAMPOR: infer() called\n";
+        auto& logit_output = get_outputs()[0];
+        allocate_tensor(logit_output, [logit_output](ov::SoPtr<ov::ITensor>& tensor) { allocate_tensor_impl(tensor, logit_output.get_element_type(), ov::Shape{1, 42}); });
     };
     std::vector<ov::ProfilingInfo> LlamaCppSyncInferRequest::get_profiling_info() const {
         std::cout << "VSHAMPOR: get_profiling_info() called\n";
--- contrib/llama_cpp_plugin/tools/runner.cpp
+++ contrib/llama_cpp_plugin/tools/runner.cpp
@@ -1,4 +1,5 @@
 #include "openvino/openvino.hpp"
+#include <cstring>
 
 int main(int argc, char* argv[]) {
     ov::Core core;
@@ -15,5 +16,43 @@ int main(int argc, char* argv[]) {
     std::cout << "VSHAMPOR: creating infer request\n";
     ov::InferRequest infer_request = compiled_model.create_infer_request();
     std::cout << "VSHAMPOR: infer request created\n";
+
+    // const ov::Output<const ov::Node>& input = compiled_model.input();
+    // std::cout << "VSHAMPOR: got input\n";
+    auto inputs = compiled_model.inputs();
+    std::cout << "VSHAMPOR: model has " << inputs.size() << " inputs\n";
+    for (const auto& input: inputs) {
+        std::cout << input.get_node()->get_friendly_name() << std::endl;
+    }
+
+    for (size_t i = 0; i < inputs.size(); i++) {
+        const auto& curr_input = inputs[i];
+        auto shape = curr_input.get_partial_shape();
+        if (shape.is_dynamic()) {
+            std::cout << "VSHAMPOR: processing input " << i << " with a dynamic shape of " << shape.to_string() << std::endl;
+            ov::Rank r = shape.rank();
+            if (r.get_length() == 2) {
+                ov::Tensor input_tensor{curr_input.get_element_type(), ov::Shape({1, 128})};
+                infer_request.set_input_tensor(i, input_tensor);
+            }
+            else {  // past_key_values
+                ov::Tensor input_tensor{curr_input.get_element_type(), ov::Shape({1, 12, 128, 64})};
+                infer_request.set_input_tensor(i, input_tensor);
+            }
+        }
+        else {
+            std::cout << "VSHAMPOR: processing input " << i << " with a non-dynamic shape of " << shape.to_string() << std::endl;
+            ov::Tensor input_tensor{curr_input.get_element_type(), curr_input.get_shape()};
+            infer_request.set_input_tensor(i, input_tensor);
+        }
+    }
+    std::cout << "VSHAMPOR: successfully set input tensor\n";
+
+    infer_request.infer();
+    std::cout << "VSHAMPOR: inferred successfully\n";
+
+    ov::Tensor output = infer_request.get_tensor("logits");
+    std::cout << "VSHAMPOR: got output tensor, shape " << output.get_shape().to_string() << std::endl;
+
     return 0;
 }
